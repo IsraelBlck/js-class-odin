553cad5fd932e9a57248beedcd7ff9f92a7f4b4c
IsraelBlck <Israelonoja21@gmail.com>
2022-09-17 16:19:24 +0100

    Get player choice validate user input and make sure case insensitive

--- index.js
+++ index.js
@@ -12,3 +12,24 @@ function getComputerChoice() {
 }
 console.log(getComputerChoice()); 
 
+
+
+// Get player choice  and case insensitive
+function getPlayerChoice() {
+    let input = prompt('Rock Paper or Scissors')
+    if( input === null || input === undefined) {
+         return prompt('Please select choice correctly')
+    }
+    input = input.toLowerCase()
+    console.log(input)
+}
+
+// Validate player choice
+function validatePlaye(choice){
+    if(gameCards(includes(choice))){
+        return true
+    }else {
+        return false
+    }
+}
+console.log(getPlayerChoice());
