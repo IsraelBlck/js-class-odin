ce817038851b37da35c6275ac4c98bbff7d13532
IsraelBlck <Israelonoja21@gmail.com>
2022-09-20 15:24:45 +0100

    validitate player choice

--- index.js
+++ index.js
@@ -1,35 +1,51 @@
 // store game items []
-let gameCards = [
-    'Rock',
-    'Paper',
-    'Scissors'
-]
+let gameCards = ["rock", "paper", "scissors"];
 
-// Get computer choice randomly
+// play game function
 
-function getComputerChoice() {
-    return gameCards[Math.floor(Math.random() * gameCards.length)]   
+function game() {
+      playRound();
 }
-console.log(getComputerChoice()); 
+// Play single round function
 
-
+function playRound() {
+  const playerSelection = getPlayerChoice();
+  const computerSelection = getComputerChoice();
+}
 
 // Get player choice  and case insensitive
+
 function getPlayerChoice() {
-    let input = prompt('Rock Paper or Scissors')
-    if( input === null || input === undefined) {
-         return prompt('Please select choice correctly')
+  input = prompt("Play PAPER, ROCK or SCISSORS");
+  while (input == null) {
+    input = prompt("Enter your choice - Paper, Rock or Scissors");
+  }
+  input = input.toLowerCase();
+  //  check = validatePlayer(input);
+  let check = validatePlayer(input);
+  while (check == false) {
+    input = prompt(
+      "Enter your choice - Paper, Rock or Scissors caps doesn't matter"
+    );
+    while (input == null) {
+      input = prompt("Enter your choice - Paper, Rock or Scissors");
     }
-    input = input.toLowerCase()
-    console.log(input)
+    input = input.toLowerCase();
+    check = validatePlayer(input);
+  }
+  console.log(input);
 }
+  
+// Get computer choice randomly
+ function getComputerChoice() {
+      return gameCards[Math.floor(Math.random() * gameCards.length)];
+ }
+ console.log(getComputerChoice());
 
 // Validate player choice
-function validatePlaye(choice){
-    if(gameCards(includes(choice))){
-        return true
-    }else {
-        return false
-    }
+function validatePlayer(choice) {
+  return gameCards.includes(choice);
 }
-console.log(getPlayerChoice());
+
+
+game()
